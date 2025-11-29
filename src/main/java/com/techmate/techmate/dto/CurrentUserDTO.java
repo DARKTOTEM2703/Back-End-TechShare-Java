abadc7cb727ac76bc4cfc4c5e6e9eabb9b2b72d0
jafeth gamboa <jafethgamboabaas@gmail.com>
2025-11-28 20:40:45 -0600

    fix: Use camelCase in CurrentUserDTO JSON output for frontend compatibility
    - Add @JsonProperty annotations to map snake_case fields to camelCase JSON
    - firstName, lastName, userName now properly serialized for React frontend
    - Fixes sidebar user display showing 'User Name' and 'Role' defaults

--- src/main/java/com/techmate/techmate/dto/CurrentUserDTO.java
+++ src/main/java/com/techmate/techmate/dto/CurrentUserDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
@@ -13,9 +14,16 @@ import java.util.List;
 @AllArgsConstructor
 public class CurrentUserDTO {
     private Integer id;
+    
+    @JsonProperty("userName")
     private String user_name;
+    
+    @JsonProperty("firstName")
     private String first_name;
+    
+    @JsonProperty("lastName")
     private String last_name;
+    
     private String email;
     private List<String> roles;
 }
